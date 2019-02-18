c7fa7c3d8182f001e23e502a8c4fe530535f0cfc
pr <>
2019-02-18 18:34:46 +0530

    SpringBoot

---
+++ Cource-api/src/main/java/pr/springboot/cource_api/CourceApiApp.java
@@ -0,0 +1,15 @@
+package pr.springboot.cource_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourceApiApp {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CourceApiApp.class, args);
+	}
+	
+
+}
---
+++ Cource-api/src/main/java/pr/springboot/cource_api/hello/HelloController.java
@@ -0,0 +1,15 @@
+package pr.springboot.cource_api.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi..Pruthwee";
+	}
+	
+	
+}
