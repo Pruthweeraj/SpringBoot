f2fcbca828f48b999ce2f30fefb9c6deb726fe40
pr <>
2019-02-19 14:55:24 +0530

    SpringBoot

--- Cource-api/src/main/java/pr/springboot/cource_api/topic/TopicController.java
+++ Cource-api/src/main/java/pr/springboot/cource_api/topic/TopicController.java
@@ -3,7 +3,10 @@ package pr.springboot.cource_api.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +14,40 @@ public class TopicController {
 
 	@Autowired
 	private TopicService topicService;
-	
-	
+
+	// Getting all topics
 	@RequestMapping("/topics")
-	public List<Topic> topics(){
-		return topicService.allTopics(); 
+	public List<Topic> topics() {
+		return topicService.allTopics();
 	}
-	
+
+	// Getting Individual topics
+	@RequestMapping("/topics/{id}")
+	public Object topic(@PathVariable String id) {
+		if (topicService.topic(id) != null) {
+			return topicService.topic(id);
+		} else {
+			return "Topic Not Found";
+		}
+	}
+
+	// Adding a topic
+	@RequestMapping(value = "/topics", method = RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		System.out.println(topic);
+		topicService.addTopic(topic);
+	}
+
+	// Updating a topic
+	@RequestMapping(value = "/topics/{id}", method = RequestMethod.PUT)
+	public void updatetopic(@PathVariable String id, @RequestBody Topic topic) {
+		topicService.updateTopic(id, topic);
+	}
+
+	// Deleting a topic
+	@RequestMapping(value = "/topics/{id}", method = RequestMethod.DELETE)
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+
 }
--- Cource-api/src/main/java/pr/springboot/cource_api/topic/TopicService.java
+++ Cource-api/src/main/java/pr/springboot/cource_api/topic/TopicService.java
@@ -1,6 +1,8 @@
 package pr.springboot.cource_api.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,15 +10,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
-			new Topic("SE" ,"Java SE" , "Java Standard Edition"),
-			new Topic("EE" ,"Java EE" , "Java Enterprice Edition"),			
-			new Topic ("Spring" , "Spring Framework" , "Spring Framework DependancyInjection & Stand alone application")
-			);
-	
-	
-	public List<Topic> allTopics(){
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(new Topic("SE", "Java SE", "Java Standard Edition"),
+			new Topic("EE", "Java EE", "Java Enterprice Edition"),
+			new Topic("Spring", "Spring Framework", "Spring Framework DependancyInjection & Stand alone application")));
+
+	// Getting all topics
+	public List<Topic> allTopics() {
 		return topics;
 	}
-	
+
+	// Getting Individual topics
+	public Topic topic(String id) {
+		Iterator<Topic> topicItr = topics.iterator();
+		while (topicItr.hasNext()) {
+			Topic topic = topicItr.next();
+			if (topic.getId().equalsIgnoreCase(id)) {
+				return topic;
+			}
+		}
+		return null;
+	}
+
+	// Adding a topic
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equalsIgnoreCase(id)) {
+				topics.set(i, topic);
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equalsIgnoreCase(id)) {
+				topics.remove(i);
+			}
+		}
+
+	}
+
 }
